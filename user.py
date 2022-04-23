2b1bad145079f6b08a9365a890059dcd59da782a
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-23 17:06:48 +0300

    Create a function that generates a password

--- user.py
+++ user.py
@@ -7,8 +7,7 @@ class User:
 
     user_createaccount = [] # create a variable that will be used to store our created user objects .
     
-    def _init_(self,first_name,last_name,email,password)
-        
+    def _init_(self,first_name,last_name,email,password):
          '''
         __init__ method that helps us define properties for our objects.
 
@@ -19,10 +18,10 @@ class User:
             password : New user password 
         '''
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
+         self.first_name = first_name
+         self.last_name = last_name
+         self.email = email
+         self.password = password
 
     def save_account(self):
         User.user_createaccount.append(self) #Create a save_account method that is called on a user object and appends it .
@@ -32,13 +31,29 @@ class User:
         '''
         generate new password
         ''' 
-        chars = '1234567890abcdefghijklmnop?/@-' #characters to choose from
-        length = int(input("Please Enter the Length of your Preferred Password: "))
-        password = ''
-        for chars in range(length):
-            password += random.choice(chars) #generate random password
-        print (password) 
-        
+        ## characters to generate password from
+        characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+        ## length of password from the user
+        length = int(input("Please Enter Preferred Password Length: "))
+
+        ## shuffling the characters
+        random.shuffle(characters)
+
+        ## picking random characters from the list
+        password = []
+        for i in range(length):
+            password.append(random.choice(characters))
+
+        ## shuffling the resultant password
+        random.shuffle(password)
+
+        ## converting the list to string
+	    ## printing the list
+        print(password)
+
+       
+
 
 
 
