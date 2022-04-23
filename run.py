9f97dd667c25e8bcb7bd0f8ceb9afb34ab8dc897
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-23 18:40:22 +0300

    Add functions that creates,saves,displays and deletes user credentials

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8 
 
 from user import User
+from user import Credentials # we import user from class credentials
 
 
 def create_account(first_name, last_name, email, password): #Function to create a new account
@@ -20,3 +21,30 @@ def generate_password(account): #function to generate password
     function to generate password
     '''
     account.generate_password()
+
+
+def create_credential(account_name, email, password): #function that creates new credentials
+
+    new_credential = Credentials(account_name, email, password)
+
+    return new_credential
+
+
+def save_credentials(credential): # function that saves credentials
+
+    credential.save_credentials()
+
+
+def save_multiple_credentials(account): #function that saves multiple credentials
+
+    account.save_multiple_credentials()
+
+
+def display_credentials(): #function that displays credentials
+    return Credentials.display_credentials()
+
+
+def delete_credentials(credential): #function to delete credentials
+    credential.delete_credentials()
+
+ 
