2b1e02f99c4d5d067a6dcfb3900506098bc9f3ea
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-23 16:31:19 +0300

    create a class that generates new instances

---
+++ user.py
@@ -0,0 +1,28 @@
+import random  #to generate the pseudo-random variables, in this case the random passwords
+
+class User:
+    """
+    Class that generates new instances of user
+    """
+
+    user_createaccount = [] # create a variable that will be used to store our created user objects .
+    
+    def _init_(self,first_name,last_name,email,password)
+        
+         '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New user first name.
+            last_name : New user last name.
+            email: New user email address.
+            password : New user password 
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+        
+
